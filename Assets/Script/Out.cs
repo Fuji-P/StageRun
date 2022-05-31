626595a18c08796715a8070c6cfead26f3482f12
Fuji-P <nyaguri0417@gmail.com>
2022-05-31 20:37:38 +0900

    初回コミット

---
+++ Assets/Script/Out.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Out : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+		{
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+    }
+}
